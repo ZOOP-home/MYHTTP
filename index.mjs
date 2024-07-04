dfc7d8f8027bdae853bd3d0d08207aa6c01a49c5
vic <d123469@gmail.com>
2024-07-04 14:41:40 +0800

    哥哥

--- index.mjs
+++ index.mjs
@@ -1,12 +1,10 @@
 import http from "http";
 
 const server = http.createServer((request, response) => {
-    response.setHeader("content-type", "text/html;charset=utf-8")
-    response.end(`hello, 你好主機, ${process.argv[2]}`)
+  response.setHeader("content-type", "text/html;charset=utf-8");
+  response.end(`hello, 你好主機, ${process.argv[2]}`);
 });
 
 server.listen(9000, () => {
-    console.log("server is running @ http://localhost:9000");
-}
-
-);
+  console.log("哥哥 加油喔 趕快來找我 @ http://localhost:9000");
+});
